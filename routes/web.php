c4e0a5dd6ff2a0fc4a6fc1685e56a315b0fa9a9f
Perry Jia <pj274@njit.edu>
2018-11-29 18:31:01 -0500

    Added: the question page that displays the answers to question

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,11 @@ Route::get('/home', 'HomeController@index')->name('home');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/user/{user_id}/profile/{profile_id}', 'ProfileController@show')->name('profile.show');
+
+Route::get('/question/{question_id}/answer/{answer_id}', 'AnswerController@show')->name('answer.show');
+
+Route::resources([
+    'questions' => 'QuestionController',
+]);
